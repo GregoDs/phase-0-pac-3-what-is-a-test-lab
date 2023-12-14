7723c46225c51893ea4265ecc213650f014a152c
Gregory <gregorykago007@gmail.com>
2023-12-14 09:05:01 +0300

    Completed assignment

--- test/root.js
+++ test/root.js
@@ -1,9 +1,18 @@
-global.expect = require('expect')
+const jsdomGlobal = require('jsdom-global');
+const path = require('path');
+const fs = require('fs');
 
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+// Set up JSDOM environment
+const dom = jsdomGlobal();
 
-jsdom({
-  src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-})
+// Load your source file
+const sourceCode = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
+eval(sourceCode);  // Evaluate the source code in the JSDOM environment
+
+// Expose 'expect' globally
+global.expect = require('expect');
+
+// Clean up after tests
+after(() => {
+  dom();  // Tear down the JSDOM environment
+});
